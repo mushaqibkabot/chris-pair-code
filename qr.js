ecd8db0965c5b82f386dbb5133e415a69ed556a0
mushaqibkabot <mushaqibkabot@gmail.com>
2025-06-11 05:38:41 -0700

    Update qr.js

--- qr.js
+++ qr.js
@@ -20,7 +20,7 @@ function removeFile(FilePath) {
 router.get('/', async (req, res) => {
     const id = makeid();
  //   let num = req.query.number;
-    async function CHRIDM_XD_PAIR_CODE() {
+    async function MUSHAqiBKA-MD-PAIR-CODE() {
         const {
             state,
             saveCreds
@@ -72,9 +72,9 @@ var randomItem = selectRandomItem(items);
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
                         let md = "malvin~" + string_session;
                         let code = await sock.sendMessage(sock.user.id, { text: md });
-                        let desc = `*Hey there, CHRISM-XD User!* 👋🏻
+                        let desc = `*Hey there, MUSHAqiBKA-MD User!* 👋🏻
 
-Thanks for using *CHRISM-XD* — your session has been successfully created!
+Thanks for using *MUSHAqiBKA-MD* — your session has been successfully created!
 
 🔐 *Session ID:* Sent above  
 ⚠️ *Keep it safe!* Do NOT share this ID with anyone.
@@ -82,24 +82,24 @@ Thanks for using *CHRISM-XD* — your session has been successfully created!
 ——————
 
 *✅ Stay Updated:*  
-Join our official WhatsApp Channel:  
-https://whatsapp.com/channel/0029Vb5qc6N2Jl8E3EcVBv0t
+Join our official WhatsApp GROUP:  
+https://chat.whatsapp.com/BivqCxXWVLGGJ5ayKeoL1x
 
 *💻 Source Code:*  
-Fork & explore the project on GitHub:  
-https://github.com/Xchristech/CHRISM-XD
+Fork & explore the project on GitHub: 
+https://github.com/mushaqibkabot/MUSHAqiBKA-MD
 
 ——————
 
-> *© Powered by Chris Gaaju*
+> *© Powered by ⟹P͙R͙A͜͡  𝗜M͙E⥃MUشAq!بKA*
 Stay cool and hack smart. ✌🏻`;
                         await sock.sendMessage(sock.user.id, {
 text: desc,
 contextInfo: {
 externalAdReply: {
-title: "ᴍᴀʟᴠɪɴ-xᴅ 𝕮𝖔𝖓𝖓𝖊𝖈𝖙𝖊𝖉",
-thumbnailUrl: "https://files.catbox.moe/g72e6z.jpg",
-sourceUrl: "https://whatsapp.com/channel/0029Vb5qc6N2Jl8E3EcVBv0t",
+title: "MUSHAqiBKA-MD",
+thumbnailUrl: "https://files.catbox.moe/umlwnw.mp4",
+sourceUrl: "https://chat.whatsapp.com/BivqCxXWVLGGJ5ayKeoL1x",
 mediaType: 1,
 renderLargerThumbnail: true
 }  
@@ -110,7 +110,7 @@ renderLargerThumbnail: true
                             let ddd = sock.sendMessage(sock.user.id, { text: e });
                             let desc = `*Hey there, CHRISM-XD User!* 👋🏻
 
-Thanks for using *CHRISM-XD* — your session has been successfully created!
+Thanks for using *MUSHAqiBKA-MD* — your session has been successfully created!
 
 🔐 *Session ID:* Sent above  
 ⚠️ *Keep it safe!* Do NOT share this ID with anyone.
@@ -118,22 +118,22 @@ Thanks for using *CHRISM-XD* — your session has been successfully created!
 ——————
 
 *✅ Stay Updated:*  
-Join our official WhatsApp Channel:  
-https://whatsapp.com/channel/0029Vb5qc6N2Jl8E3EcVBv0t
+Join our official WhatsApp GROUP:  
+https://chat.whatsapp.com/BivqCxXWVLGGJ5ayKeoL1x
 
 *💻 Source Code:*  
 Fork & explore the project on GitHub:  
-https://github.com/Xchristech/CHRISM-XD
+https://github.com/mushaqibkabot/MUSHAqiBKA-MD
 
-> *© Powered by Chris Gaaju*
+> *© Powered by ⟹P͙R͙A͜͡  𝗜M͙E⥃MUشAq!بKA*
 Stay cool and hack smart. ✌🏻*`;
                             await sock.sendMessage(sock.user.id, {
 text: desc,
 contextInfo: {
 externalAdReply: {
-title: "ᴍᴀʟᴠɪɴ-xᴅ 𝕮𝖔𝖓𝖓𝖊𝖈𝖙𝖊𝖉 ✅  ",
-thumbnailUrl: "https://files.catbox.moe/g72e6z.jpg",
-sourceUrl: "https://whatsapp.com/channel/0029Vb5qc6N2Jl8E3EcVBv0t",
+title: " MUSHAqiBKA-MD CONNECTED ✅  ",
+thumbnailUrl: "https://files.catbox.moe/umlwnw.mp4",
+sourceUrl: "https://chat.whatsapp.com/BivqCxXWVLGGJ5ayKeoL1x",
 mediaType: 2,
 renderLargerThumbnail: true,
 showAdAttribution: true
@@ -150,7 +150,7 @@ showAdAttribution: true
                     process.exit();
                 } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
                     await delay(10);
-                    CHRISM_XD_PAIR_CODE();
+                    MUSHAqiBK-MD-PAIR-CODE();
                 }
             });
         } catch (err) {
@@ -161,7 +161,7 @@ showAdAttribution: true
             }
         }
     }
-    await CHRISM_XD_PAIR_CODE();
+    await MUSHAqiBKA-MD-PAIR-CODE();
 });
 setInterval(() => {
     console.log("☘️ 𝗥𝗲𝘀𝘁𝗮𝗿𝘁𝗶𝗻𝗴 𝗽𝗿𝗼𝗰𝗲𝘀𝘀...");
